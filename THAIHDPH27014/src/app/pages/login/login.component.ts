7ac1e05ce5e480b97bdc9e61deaf5eef3fb2a9a7
thaihdph27014 <hoangducthai2301@gmail.com>
2023-06-08 18:42:56 +0700

    fn

--- THAIHDPH27014/src/app/pages/login/login.component.ts
+++ THAIHDPH27014/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private route: Router) {
+  }
+
+  authForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  })
+
+  onSubmit() {
+    const data = this.authForm.value;
+    this.authService.signIn(data).subscribe((ressponse) => {
+      alert("Dang nhap tai khoan thanh cong!")
+      const user = {
+        accessToken: ressponse.accessToken,
+        user: ressponse.user
+      }
+      
+      localStorage.setItem(
+        'user',
+        JSON.stringify(user)
+      );
+      this.route.navigateByUrl('/')
+    }, error => {
+      alert(error.error.message)
+      
+    })
+    
+    
+  }
 }
