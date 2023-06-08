21ba5cefc2af0e6a22e2eae8656ce38eceb0a377
thaihdph27014 <hoangducthai2301@gmail.com>
2023-06-08 14:24:29 +0700

    dky

--- THAIHDPH27014/src/app/pages/register/register.component.ts
+++ THAIHDPH27014/src/app/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {  OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'register',
@@ -8,16 +9,39 @@ import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } 
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent  {
-  user = {
-    username: '',
-    email: '',
-    password: '',
-    confirmpassword:'',
-  };
+  
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private route: Router) {
+
+  }
+
+  authForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+  }, {
+    validator: this.passwordMatchValidator,
+  })
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+    
+    if (password !== confirmPassword) {
+      formGroup.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+    } else {
+      formGroup.get('confirmPassword')?.setErrors(null);
+    }
+  }
 
   onSubmit() {
-    console.log('Register complete!!', this.user);
-    // Goi API
+    const data = this.authForm.value;
+    this.authService.signUp(data).subscribe(() => {
+      alert("Dang ki tai khoan thanh cong!")
+      this.route.navigateByUrl('/login')
+    })
+    
+    
   }
   // validateForm!: UntypedFormGroup;
   // captchaTooltipIcon: NzFormTooltipIcon = {
